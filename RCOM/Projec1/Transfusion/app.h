4bd22a68bd07558aca12d80e8a205b09e75e31b8
LeToze <50966869+LeToze@users.noreply.github.com>
2019-10-23 23:22:46 +0100

    Add files via upload

---
+++ RCOM/Projec1/Transfusion/app.h
@@ -0,0 +1,10 @@
+int ControlPackage(int c);
+int setFile();
+int PingOP();
+int DataPackage(int datalength);
+int sendData();
+int sendControlPacket(unsigned char control_byte);
+int receiveData();
+int sendPacket(int seqNumber, unsigned char* buffer, int length);
+int receiveControlPacket();
+int receivePacket(unsigned char** buffer, int seqNumber);
