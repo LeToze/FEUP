254793e40fddf02b391c6458840131562aacce4d
LeToze <50966869+LeToze@users.noreply.github.com>
2019-10-23 16:11:12 +0100

    Delete datalink.h

--- datalink.h
+++
@@ -1,5 +0,0 @@
-#include "datalink.c"
-int llopen(char port[], int mode);
-int llwrite(int fd, char * buffer, int length, int mode);
-int llread(int fd, char * buffer, int mode);
-int llclose(int fd, int mode);
