4bac329dd7ec46922bdbe71ef611dff22852fa41
LeToze <50966869+LeToze@users.noreply.github.com>
2019-10-28 11:48:04 +0000

    Add files via upload

---
+++ RCOM/Projec1/Final_Day/1/app.h
@@ -0,0 +1,19 @@
+int ControlPackage(int c);
+int setFile();
+int PingOP();
+int DataPackage(int datalength);
+int sendData();
+int sendControlPacket(unsigned char control_byte);
+int receiveData();
+int sendPacket(int seqNumber, unsigned char* buffer, int length);
+int receiveControlPacket();
+int receivePacket(unsigned char* buffer, int seqNumber);
+void go(int mode);
+
+struct applicationLayer {
+	int fileDescriptor; /*Descritor correspondente à porta série*/
+	int status; /*TRANSMITTER | RECEIVER*/
+  int fd;
+	int fragmentSize;
+};
+ struct applicationLayer applay;
