449862c35c2ed43f0c3e230deadc2908c5866231
LeToze <50966869+LeToze@users.noreply.github.com>
2019-10-29 05:07:01 +0000

    Add files via upload

---
+++ RCOM/Projec1/Final_Day/Inthemiddleofthenight/ola/app.c
@@ -0,0 +1,376 @@
+#include "datalink.h"
+#include "app.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+//#include <io.h>
+#include <signal.h>
+#define CONTROLEND 0x03
+#define CONTROLSTART 0x02
+#define CONTROLDATA 0X01
+#define CONTROLT1 0X00
+#define CONTROLT2 0X01
+
+
+struct controlpack {
+	int filesize;
+	unsigned char filename[50];
+	unsigned char data[255];
+};
+
+struct datapack {
+	int SeqNum;
+	int DataSize;
+	unsigned char data[255];
+};
+
+unsigned char DATA[255];
+struct controlpack c1;
+struct datapack d1;
+int count=0; //number of packets received
+
+void go(int mode){
+	if(mode == TRANSMITTER){
+		sendData();
+	}
+	if(mode == RECEIVER){
+		receiveData();
+	}
+}
+
+/*int ControlPackage(int c){
+	int j = 0;
+	c1.filesize = 0;
+	strcpy(c1.filename,"pinguim");
+	c1.data[0] = c;// 2 se start,3 se end
+	c1.data[1] = 1;
+	for ( j = 0;j < strlen(c1.filename)+1;j++) {
+		c1.data[j+3] = c1.filename[j];
+	}
+	c1.data[2] = strlen(&c1.data[3]) + 1;
+	c1.data[j+4] = 0;
+	c1.data[j+5] = c1.filesize;
+	c1.data[j+6] = strlen(&c1.data[3]) + 1;
+	if(llwrite(applay.fd,c1.data,1,TRANSMITTER) < 0){
+		printf("Error 45\n");
+		return -1;
+	}
+return 0;
+}*/
+
+int setFile() {
+
+  if(c1.filename == NULL) {
+    printf("Filename is null\n");
+    return -1;
+  }
+
+  if((applay.fileDescriptor = open(c1.filename, O_RDONLY)) < 0) {
+    perror("Error opening the file");
+    return -1;
+  }
+
+  return 0;
+}
+
+int PingOP(){
+	printf("c1.filename:%s",c1.filename);
+	if(open(c1.filename,O_CREAT|O_WRONLY|O_APPEND, S_IWUSR|S_IRUSR) < 0){
+		perror("Error opening the file");
+		return -1;
+	}
+
+	return 0;
+}
+
+/*int DataPackage(int datalength) {
+	int i = 0;
+	d1.data[0] = 1;
+	d1.SeqNum = 0;
+	d1.data[1]= 0;
+	d1.data[2] = datalength % 256;
+	d1.data[3] = datalength / 256;
+	memcpy(&d1.data[4],data.frame,datalength);
+	if(llwrite(applay.fileDescriptor,d1.data,46,TRANSMITTER) < 0){
+		printf("Error 46\n");
+		return -1;
+	}
+	for (i = 0;i < datalength;i++) {
+		d1.data[2 + j] = DATA[i];
+	}
+	return 0;
+}*/
+
+int sendData() {
+	int i=0;
+  if (sendControlPacket(CONTROLSTART) < 0) {
+    printf("Error in sendControlPacket\n");
+    return -1;
+  }
+
+  i++;
+  applay.fragmentSize=255;
+  int bytesRead = 0, seqNumber = 1000;
+  unsigned char * buffer = (unsigned char *) malloc(applay.fragmentSize + 1);
+
+  while ((bytesRead = read(applay.fileDescriptor, buffer, applay.fragmentSize)) > 0) {
+    i++;
+    if (sendPacket(seqNumber, buffer, bytesRead) < 0) {
+      printf("Error in sendPacket\n");
+      free(buffer);
+      return -1;
+    }
+
+     ++seqNumber;
+     seqNumber %= 255;
+  }
+
+  if (sendControlPacket(CONTROLEND) < 0) {
+    printf("Error in sendControlPacket\n");
+    return -1;
+  }
+
+  i++;
+	//memcpy(&data.frame,&buffer,bytesRead);
+    if (close(applay.fileDescriptor) < 0) {
+    printf("Error closing the file.\n");
+    return -1;
+  }
+
+  llclose(applay.fd,TRANSMITTER);
+  return 0;
+}
+
+int sendControlPacket(unsigned char control_byte) {
+
+
+	if (control_byte == 0x02) {
+		if (setFile() < 0) {
+			printf("Error getting the file\n");
+			return -1;
+		}
+	}
+	struct stat f_information;
+
+	if (fstat(applay.fileDescriptor, &f_information) < 0) {
+		perror("Couldn't obtain information regarding the file");
+		return -1;
+	}
+
+
+	off_t f_size = f_information.st_size; /* total size in bytes, in signed integer */
+	c1.filesize = f_size;
+
+	unsigned int l1 = sizeof(f_size);
+	unsigned int l2 = strlen(c1.filename) + 1;
+
+	int startpackage_len=5+l1+l2;
+
+	unsigned char startpackage[startpackage_len];
+	startpackage[0] = control_byte; // CONTROLSTART or CONTROLEND
+	startpackage[1] = CONTROLT1;
+	startpackage[2] = l1;
+	*((off_t*)(startpackage + 3)) = f_size;
+	startpackage[3 + l1] = CONTROLT2;
+	startpackage[3 + l1 + 1] = l2; /* +1 from CONTROLT2 */
+
+	strcat((unsigned char*)startpackage + 5 + l1, c1.filename);
+	fflush(stdout);
+	printf("Filename:%s\n",c1.filename);
+	fflush(stdout);
+	if (llwrite(applay.fd, startpackage,startpackage_len,TRANSMITTER) < 0) {
+		printf("Couldn't write control package.\n");
+		return -1;
+	}
+	return 0;
+}
+
+int sendPacket(int seqNumber, unsigned char* buffer, int length) {
+
+	int totalLength = length + 4;
+	unsigned char dataPacket[255];
+
+	dataPacket[0] = CONTROLT2;
+	dataPacket[1] = seqNumber;
+	dataPacket[2] = length / 256;
+	dataPacket[3] = length % 256;
+
+	memcpy(&dataPacket[4], buffer, length);
+
+	if (llwrite(applay.fd, dataPacket, totalLength,TRANSMITTER) < 0) {
+		printf("Error sending data packet");
+		return -1;
+	}
+
+	return 0;
+}
+
+int receiveData() {
+int i=0;
+  if (receiveControlPacket() < 0) {
+    printf("error in receiveControlPacket\n");
+    return -1;
+  }
+  printf("Recebi um CT packet\n");
+  i++;
+
+  int bytesRead = 0, seqNumber = 0, counter = 3;
+  unsigned char buffer[255];
+	unsigned char inutil[20];
+  while(counter < c1.filesize) {
+		fflush(stdout);
+    i++;
+
+    bytesRead = receivePacket(buffer, seqNumber);
+    if(bytesRead < 0) {
+      printf("receivePacket didn't read \n");
+      continue;
+    }
+		  counter+= bytesRead;
+			printf("\nCounter: %d\n", counter);
+			PingOP();
+      printf("Ficheiro Criado\n");
+		if(write(applay.fileDescriptor, buffer, bytesRead) <= 0) {
+      perror("Couldn't write to file\n");
+    }
+    seqNumber++;
+    //free(buffer);
+    }
+		llread(applay.fd ,inutil,3,RECEIVER);
+		printf("Recebi os DT packet\n");
+
+  if (receiveControlPacket() < 0) {
+    printf("Error in receiveControlPacket\n");
+    return -1;
+  }
+
+  	i++;
+
+    if (close(applay.fileDescriptor) < 0) {
+    printf("Error closing the file.\n");
+    return -1;
+  }
+  llclose(applay.fd,RECEIVER);
+  system("clear"); //*nix
+  printf("<<< Finished >>>\n");
+  return 0;
+}
+
+int receiveControlPacket() {
+	unsigned char *read_package=malloc(sizeof(unsigned char)*255);
+	unsigned int package_size = llread(applay.fd ,(read_package),25,RECEIVER);
+	fflush(stdout);
+
+	if (package_size < 0) {
+		perror("Couldn't read linklayer whilst receiving package.");
+		return -1;
+	}
+
+	int pck_index = 0;
+
+	if (read_package[0] == CONTROLEND) {
+		free(read_package);
+		printf("End Package received\n");
+		return 0;
+	} /*End of transfer process, nothing to process any further.*/
+
+	pck_index++; //move on to T1
+	unsigned int n_bytes;
+
+	unsigned char pck_type; //T
+	for (int i = 0; i < 2; i++) {
+		pck_type = read_package[pck_index++]; // read T, update to L
+		switch (pck_type) {
+		 case CONTROLT1:
+			n_bytes = (unsigned int) (read_package[pck_index++]); // read L1, update to V1;
+			c1.filesize = *((off_t*)(read_package + pck_index));
+			pck_index += n_bytes; //update to T2
+	printf("\nfileNAME %s    fileSIZE%d\n",c1.filename,n_bytes );fflush(stdout);
+			break;
+
+		case CONTROLT2:
+			n_bytes = (unsigned int)read_package[pck_index++]; // read L2, update to V2
+			memcpy(c1.filename, (char*)&read_package[pck_index + 1], n_bytes + 1); /* Transfering block of memory to a.layer's filename */
+			printf("\nfileNAME %s    fileSIZE%d\n",c1.filename,n_bytes );fflush(stdout);
+			PingOP();
+      printf("Ficheiro Criado\n");
+			break;
+
+		default:
+			printf("\nfileNAME %s    fileSIZE%d\n",c1.filename,n_bytes );fflush(stdout);
+			printf("T: %x \n", pck_type);
+			printf("T parameter in start control packet couldn't be recognised, moving ahead...\n");
+		}
+	}
+
+	free(read_package);
+	return 0;
+}
+
+int receivePacket(unsigned char* buffer, int seqNumber) {
+
+	unsigned char* information=malloc(sizeof(char)*255);
+	int K = 0; // number of octets
+
+	if (llread(applay.fd,information,255,RECEIVER) < 0) {
+		printf("error in llread\n");
+		return -1;
+	}
+
+	++count;
+  fflush(stdout);
+
+  printf("Pacote nº: %d recebido\n",count);
+  fflush(stdout);
+
+  if(count ==44){
+    if (llread(applay.fd,information,3,RECEIVER) < 0) {
+  		printf("error in llread\n");
+  		return -1;
+  	}
+    fflush(stdout);
+    printf("Ultimo pacote\n");
+    fflush(stdout);
+  }
+
+	//printf("Já recebi %d pacotes\n",count);
+
+	if (information == NULL) {
+		printf("receivePacket: information = NULL\n");
+		return -1;
+	}
+
+	unsigned char Ch = information[0]; // control field
+	int N = (information[1]); // sequence number
+	printf("O N tem o valor de %d",N);
+
+	if (Ch != CONTROLT2) {
+		printf("Ch: %x\n", Ch);
+		printf("receivePacket: C doesn't indicate data\n");
+		return -1;
+	}
+
+	if (N != seqNumber) {
+		printf("\nSeqNumber:%d\n",seqNumber);
+		printf("receivePacket: wrong sequence number\n");
+		return -1;
+	}
+
+	int L2 = information[2];
+	int L1 = information[3];
+	K = 256 * L2 + L1;
+
+	buffer = (unsigned char*)malloc(K);
+	memcpy((buffer),(information + 4), K);
+
+	free(information);
+  // fflush(stdout);
+  // printf("Valor de K: %d",K);
+  // fflush(stdout);
+	return K;
+}
---
+++ RCOM/Projec1/Final_Day/Inthemiddleofthenight/ola/datalink.c
@@ -0,0 +1,749 @@
+#include "datalink.h"
+#include "app.h"
+int A = 3;
+
+
+
+volatile int STOP=FALSE;
+int LIDO=FALSE;
+int ABERTO=FALSE;
+int big_estado=0;
+int RRDONE=FALSE;
+int DISCDONE= FALSE;
+int IDONE = FALSE;
+int REJDONE = FALSE;
+
+int fd,c, res;
+struct termios oldtio,newtio;
+int sender;
+struct linkLayer data;
+unsigned char buf[255];
+unsigned char buf2[255];
+unsigned char dbcc=0x00;//data BCC
+unsigned char ESC_BYTE = 0x7d;
+unsigned char set_buf[SIZE];
+unsigned char rec[255];
+int i, sum = 0,fseq=0,rcv=0;
+int estado=0,speed =0;
+
+void atende()                   // atende alarme
+{
+  int flag=1, conta=1;
+	printf("%d\n", conta);
+	flag=1;
+	conta++;
+}
+
+void maq_estados(unsigned char c,unsigned char sign, int mode){
+
+  fflush(stdout);
+  printf(" Recebi o caracter %x  no estado %d \n",c,estado);
+
+  if(mode == TRANSMITTER){
+	  A=0x01;
+  }
+  if(mode == RECEIVER){
+	  A=0x03;
+  }
+
+  unsigned char BCC = A ^ sign;
+  printf("A = %x",A);
+  switch(estado){
+    case 0:
+      if (c==FLAG){
+        estado=1;
+        break;
+      }
+    case 1:
+      if (c == A){
+        estado=2;
+        break;
+      }
+      else if( c == sign || c == BCC){
+        estado=0;
+        break;
+      }
+      else if(c ==FLAG){
+        estado=1;
+        break;
+      }
+    case 2:
+      if (c == sign){
+        estado=3;
+        break;
+      }
+      else if( c== A|| c == BCC){
+        estado=0;
+        break;
+      }
+      else if(c == FLAG){
+        estado=1;
+        break;
+      }
+    case 3:
+      if (c==BCC){
+        estado=4;
+        break;
+      }
+      else if( c== A || c == sign){
+        estado=0;
+        break;
+      }
+      else if(c == FLAG){
+        estado=1;
+        break;
+      }
+    case 4:
+      if (c==FLAG){
+        estado=5;
+
+		if (sign == SET) {
+			STOP = TRUE;
+		}
+		else if (sign == DISC) {
+			DISCDONE = TRUE;
+		}
+		else if (sign == UA) {
+			LIDO = TRUE;
+		}
+		else if (sign == RR0 || sign == RR1) {
+			RRDONE = TRUE;
+		}
+		else if (sign == REJ0 || sign == REJ1) {
+			REJDONE = TRUE;
+		}
+		estado = 0;
+        break;
+      }
+      else if( c== A || c==sign || c== BCC){
+        estado=0;
+        break;
+      }
+  }
+}
+
+int trama(int mode,char frame[]){
+	unsigned char set_buf[50];
+  int j=0;
+  if(mode == TRANSMITTER){
+	  A = 0x03;
+  }
+  if(mode == RECEIVER){
+	  A = 0x01;
+  }
+  set_buf[0] = FLAG;//F
+	set_buf[1] = A;
+	set_buf[2] = data.sequenceNumber;
+	set_buf[3] = set_buf[1] ^ set_buf[2]; //0x00
+  //set_buf[4] = FLAG;//
+  for(j=0;frame[j] != '\0';j++){
+    set_buf[j+4]=frame[j];
+  }
+}
+
+unsigned char *Destuffing (unsigned char *frame,int length){
+	//int length = strlen(frame);
+	unsigned char *stuffedFrame=malloc(sizeof(unsigned char)*length+1);
+  //unsigned char stuffedFrame[length];
+	int i,j=0,z=0,g;
+  unsigned char bcc;
+
+/*  for(z=0;z < length ;z++){
+    stuffedFrame[z] =stuffedFrame[z+4];
+}*/
+printf("FRAME:%x",frame[0]);
+
+	for(i=0;i < length-2-4 ;i++,j++){
+		if(frame[i] == ESC_BYTE){
+			stuffedFrame[j] = frame[i+1]^0x20;
+			i++;
+		}
+		else{
+			stuffedFrame[j] = frame[i];
+      bcc^=stuffedFrame[j];
+		}
+	}
+
+  if(bcc == 0){
+    fflush(stdout);
+    printf("Destuffing Sucedido\n");
+    fflush(stdout);
+  }
+  fflush(stdout);
+  printf("VALOR DO Stuffed :%x",stuffedFrame[0]);
+  fflush(stdout);
+  printf("VALOR DO Stuffed :%x",stuffedFrame[1]);
+  fflush(stdout);
+  printf("VALOR DO BCC:%x",bcc);
+  fflush(stdout);
+  fflush(stdin);
+  //sleep(1);
+
+	return stuffedFrame;
+}
+
+
+int maq_estados_I(unsigned char c, int mode) {
+	printf(" Recebi o caracter %x  no estado %d \n", c, estado);
+	int fseq;
+	unsigned char buffer[255];
+	int k = 0;
+
+if(mode == TRANSMITTER){
+	  A=0x01;
+  }
+  if(mode == RECEIVER){
+	  A=0x03;
+  }
+
+	switch (estado) {
+	case 0:
+		if (c == FLAG) {
+
+			estado = 1;
+			break;
+		}
+	case 1:
+		if (c == A) {
+
+			estado = 2;
+			break;
+		}
+		else if (FLAG) {
+			estado = 1;
+			break;
+		}
+		else  {
+			estado = 0;
+			break;
+		}
+	case 2:
+		if (c == SEQ_0) {
+			estado = 3;
+			fseq = 0;
+			break;
+		}
+		else if (c == SEQ_1) {
+			estado = 3;
+			fseq = 1;
+			break;
+		}
+		else if (FLAG) {
+			estado = 1;
+			break;
+		}
+		else {
+			estado = 0;
+			break;
+		}
+	case 3:
+		if (!fseq) {
+			if (c == (A ^ SEQ_0)) {
+				estado = 4;
+				break;
+			}
+		}
+		else if (fseq) {
+			if (c== (A ^ SEQ_1)) {
+				estado = 4;
+				break;
+			}
+		}
+		else if (FLAG) {
+			estado = 0;
+			break;
+		}
+		else {
+			estado=0;
+		}
+	case 4:
+		if (c == FLAG) {
+			estado = 1;
+			break;
+		}
+		else if (c == dbcc) {
+			estado = 6;
+			break;
+		}
+		else if (c == ESC_BYTE) {
+			estado = 5;
+			break;
+		}
+		else { //Cria o BCC
+			buffer[k] = c;
+			k++;
+			dbcc = dbcc ^ c;
+			break;
+		}
+	case 5:
+		if (c == 0x5e) {
+			buffer[i++] = 0x7e;
+			if (dbcc == FLAG) {
+				estado = 6;
+				break;
+			}
+			dbcc = 0x7e ^ dbcc;
+			estado = 4;
+		}
+		else if (c == 0x5d) {
+			buffer[i++] = ESC_BYTE;
+			if (dbcc == ESC_BYTE) {
+				estado = 6;
+				break;
+			}
+			dbcc = ESC_BYTE ^ dbcc;
+			estado = 4;
+		}
+		else {
+			buffer[i++] = ESC_BYTE;
+			dbcc = ESC_BYTE ^ dbcc;
+
+			if (c == dbcc) {
+				estado = 6;
+				break;
+			}
+			else if (c == FLAG) {
+				estado = 1;
+				break;
+			}
+			else if (c == ESC_BYTE) {
+				estado = 5;
+				break;
+			}
+			else {
+				buffer[i++] = c;
+				dbcc = c ^ dbcc;
+				estado = 4;
+				break;
+			}
+		}
+	case 6:
+		if (c == FLAG) {
+			estado = 0;
+      	    ++rcv;
+			IDONE = TRUE;
+			break;
+		}
+		else {
+			buffer[i++] = c;
+			dbcc = c ^ dbcc;
+			if (c == ESC_BYTE) {
+				estado = 5;
+				break;
+			}
+			else if (c == dbcc) {
+				estado = 6;
+				return fseq;
+				break;
+			}
+			else if (c == FLAG) {
+				estado = 1;
+				break;
+			}
+			else {
+				buffer[i++] = c;
+				dbcc = c ^ dbcc;
+			}
+		}
+
+	}
+	for(int j=0;j <strlen(buffer)+1 ;j++){
+		fflush(stdout);
+		printf("BUFFER final:%x\n",buffer[j]);
+		fflush(stdout);
+	}
+
+	return 0;
+}
+
+/*void big_maq_estados(){
+printf("%d\n\n\n\n", big_estado);
+switch(big_estado){
+  case '0':
+    if (ABERTO){
+      big_estado=1;
+    }
+  case '1':
+    if (LIDO){
+      big_estado=2;
+    }
+  case '2':
+    exit(1);
+    }
+}*/
+
+int send_I(int fd,unsigned char frame[]) {
+	unsigned char set_buf[MAX_SIZE];
+	unsigned char dbcc;
+	int size;
+	set_buf[0] = FLAG;
+	set_buf[1] = A;
+	printf("Fseq=%d",fseq);
+	if (fseq) {
+		set_buf[2] = 0x20;
+	}
+	if (!fseq) {
+		set_buf[2] = 0x00;
+	}
+	set_buf[3] = set_buf[1] ^ set_buf[2];//i percorre set_buf
+	for (int j = 0,i = 4; j < strlen(frame)+1;i++,j++) {
+		dbcc ^= set_buf[i];
+		if (frame[j] == 0x7E) {
+			set_buf[i] = ESC_BYTE;
+			i++;
+			set_buf[i] = 0x5E;
+		}
+		else if (frame[j] == ESC_BYTE) {
+			set_buf[i] = ESC_BYTE;
+			++i;
+			set_buf[i] = 0x5D;
+		}
+		else {
+			set_buf[i] = frame[j];
+		}
+	}
+	if (dbcc != FLAG && dbcc != ESC_BYTE) {
+		set_buf[i] = dbcc;
+		set_buf[i + 1] = FLAG;
+		set_buf[i + 2] = '\0';
+	}
+	size = strlen(set_buf);//size of I
+	for (i = 0;i < 5;i++) {
+		write(fd, &set_buf[i],1);
+	}
+	return 0;
+}
+
+int send(int fd,int sign, int mode){
+	unsigned char set_buf[5];
+  if(mode == TRANSMITTER){
+	  A = 0x03;
+  }
+  if(mode == RECEIVER){
+	  A = 0x01;
+  }
+  set_buf[0] = FLAG;//F
+	set_buf[1] = A;
+	set_buf[2] = sign;
+	set_buf[3] = set_buf[1] ^ set_buf[2]; //0x00
+  set_buf[4] = FLAG;//F
+
+      for (i = 0; i < 5; i++) {
+           write(fd,&set_buf[i],1); // carater a carater e o /0 j� t� no buf na ultima posi��o
+      }
+    return 1;
+}
+
+int main(int argc, char** argv)
+{
+    strcpy(data.port, argv[1]);
+    data.baudRate = BAUDRATE;
+    data.timeout = 3;
+
+ 	applay.fd  = open(argv[1], O_RDWR | O_NOCTTY );
+	fd=applay.fd;
+
+  if (fd <0) {perror(argv[1]); exit(-1); }
+
+  if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+     perror("tcgetattr");
+     exit(-1);
+   }
+
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
+
+   /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
+
+  newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+  tcflush(fd, TCIOFLUSH);
+
+  if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+   printf("New termios structure set\n");
+   llopen(argv[1],RECEIVER);
+
+	go(RECEIVER);
+
+    if ( (argc < 2) ||
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
+
+    (void) signal(SIGALRM, atende);
+	llclose(applay.fd,RECEIVER);
+}
+
+int llopen(unsigned char port[],int mode) {
+   printf("MODE:%d\n",mode);
+  if(mode == TRANSMITTER){
+	send(fd,SET,mode);
+	printf("Cheguei\n");
+	while(LIDO == FALSE){
+			res = read(fd,buf,1);
+			buf[res]='\0';
+			printf(":%x:%d\n", buf[0], res);
+			maq_estados(buf[0],UA,TRANSMITTER);
+  		}
+  }
+
+   if(mode == RECEIVER){
+   		while (STOP==FALSE) {       /* loop for input */
+        	res = read(fd,buf,1);   /* returns after 5 chars have been input */
+        	buf[res]='\0';               /* so we can printf... */
+        	printf(":%x:%d\n", buf[0], res);
+        	if (buf[0]=='z') STOP=TRUE; /*linha que para a leitura de carateres*/
+  	    	maq_estados(buf[0],SET,RECEIVER);
+      }
+
+  send(fd,UA,RECEIVER);
+  }
+
+  ABERTO = TRUE;
+  printf("Conexão Efetuada com sucesso\n");
+  return fd;
+}
+
+int llwrite(int fd, unsigned char * buffer, int length,int mode){
+
+unsigned char buf[20];
+
+if(mode == RECEIVER){
+	printf("llwrite não funciona em receiver mode\n");
+	exit(1);
+}
+send_I(fd, buffer);
+
+while (!(estado == 5)) {       /* loop for input */
+	res = read(fd, buf, 1);   /* returns after 5 chars have been input */
+	buf[res] = '\0';               /* so we can printf... */
+	printf(":%x:%d\n", buf[0], res);
+	fflush(stdout);
+	fseq=0;
+	if(fseq){
+	maq_estados(buf[0], RR1,TRANSMITTER);
+	}
+	if(!fseq){
+	maq_estados(buf[0], RR0,TRANSMITTER);
+	}
+}
+
+return 0;
+}
+
+int llread(int fd, unsigned char *buffer,int length,int mode){
+  unsigned char buf[50];
+  int i=0;
+  A=0x03;
+  fflush(stdout);
+
+if(mode == TRANSMITTER){
+	printf("llread não funciona em transmiter mode\n");
+	exit(1);
+}
+
+while (IDONE == FALSE) {
+  if((res = read(fd,&buf[0],1)) < 0){
+    fflush(stdout);
+    printf("Reading Error\n");
+    fflush(stdout);
+  }
+	//read(fd,&buf[0],1);
+	buf[res] = '\0';
+	//buffer[i++]=buf[0];
+	c=buf[0];
+
+//printf(" Recebi o caracter %x  no estado %d \n",c,estado);
+switch (estado) {
+	case 0:
+		if (c == FLAG) {
+			estado = 1;
+			break;
+		}
+	case 1:
+		if (c == A) {
+
+			estado = 2;
+			break;
+		}
+		else if (FLAG) {
+			estado = 1;
+			break;
+		}
+		else  {
+			estado = 0;
+			break;
+		}
+	case 2:
+		if (c == SEQ_0) {
+			estado = 3;
+			fseq = 0;
+			break;
+		}
+		else if (c == SEQ_1) {
+			estado = 3;
+			fseq = 1;
+			break;
+		}
+		else if (c == FLAG) {
+			estado = 1;
+			break;
+		}
+		else {
+			estado = 0;
+			break;
+		}
+	case 3:
+		if (!fseq) {
+			if (c == (A ^ SEQ_0)) {
+				estado = 4;
+        //i=0;
+				break;
+			}
+		}
+		else if (fseq) {
+			if (c== (A ^ SEQ_1)) {
+				estado = 4;
+        //i=0;
+				break;
+			}
+		}
+		else {
+			estado=0;
+			break;
+		}
+	case 4:
+	if (c == FLAG) {
+		if (createBCC2(buffer,i)) {
+			if(fseq){
+				send(fd,(unsigned char)RR1,RECEIVER);//RR1
+			}
+			else{
+				send(fd,(unsigned char)RR0,RECEIVER);//RR0
+			}
+			estado = 0;
+			IDONE=TRUE;
+			sender=1;
+			break;
+		}
+		else{
+			if(fseq){
+				send(fd,(unsigned char)REJ1,RECEIVER);//REJ1
+			}
+			else{
+				send(fd,(unsigned char)REJ0,RECEIVER);//REJ0
+			}
+			estado = 0;
+			IDONE=TRUE;
+			sender=0;
+			break;
+		}
+	}
+	else if (c == ESC_BYTE) {
+			estado = 5;
+			break;
+	}
+	else {
+		buffer[i++]=c;
+		break;
+	}
+	case 5:
+		if(c == 0x5e){
+			buffer[i++] = FLAG;
+      //buffer[i]='\0';
+		}
+		else{
+			if(c == 0x5d){
+				buffer[i++]=ESC_BYTE;
+        //buffer[i]='\0';
+			}
+			else{
+				printf("Not valid");
+				exit(-1);
+			}
+		}
+		estado=4;
+		break;
+	}
+}
+
+//printf("Valor do i:%d\n",strlen(buffer));
+//strcpy(buffer,Destuffing(buffer,length));
+//printf("YTAWDFAWED %d\n",i);fflush(stdout);
+for(int y=0;y < i;y++){
+  fflush(stdout);
+	printf("Stufed:%x\t",buffer[y]);
+  fflush(stdout);
+	//buffer[y+4]=0;
+}
+
+
+IDONE=FALSE;
+
+return 0;
+}
+
+
+
+int createBCC2(unsigned char *mess,int size){
+	int i=1;
+	unsigned char BCC2;
+	BCC2=mess[0];
+	for(i=1;i < size;i++){
+		BCC2 ^= mess[i];
+	}
+  printf("BCC: %x\n",BCC2);fflush(stdout);
+	if(BCC2 == 0/*mess[size-1]*/){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+
+int llclose(int fd,int mode){
+	unsigned char buf[255];
+	if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+      perror("tcsetattr");
+  }
+
+  if(mode == TRANSMITTER){
+	send(fd,DISC,TRANSMITTER);
+	while (DISCDONE==FALSE) {       /* loop for input */
+        	res = read(fd,buf,1);   /* returns after 5 chars have been input */
+        	buf[res]='\0';               /* so we can printf... */
+        	printf(":%x:%d\n", buf[0], res);
+  	    	maq_estados(buf[0],DISC,TRANSMITTER);
+      }
+		send(fd,UA,TRANSMITTER);
+  }
+
+  if(mode == RECEIVER){
+	while (DISCDONE==FALSE) {       /* loop for input */
+        	res = read(fd,buf,1);   /* returns after 5 chars have been input */
+        	buf[res]='\0';               /* so we can printf... */
+        	printf(":%x:%d\n", buf[0], res);
+  	    	maq_estados(buf[0],DISC,RECEIVER);
+      }
+
+		send(fd,DISC,RECEIVER);
+		while (LIDO == FALSE) {       /* loop for input */
+			res = read(fd, buf, 1);   /* returns after 5 chars have been input */
+			buf[res] = '\0';               /* so we can printf... */
+			printf(":%x:%d\n", buf[0], res);
+			maq_estados(buf[0],UA,RECEIVER);
+		}
+  }
+	printf("\nFoi terminada a conexao\n");
+    close(fd);
+    return 0;
+}
